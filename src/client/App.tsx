fce7668fa390849ee62b6fefc950167c01b7be16
otofu-square <yukita@feedforce.jp>
2017-06-30 02:04:52 +0900

    :tada: Add landing page

---
+++ src/client/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const App = () =>
+  <div className="container">
+    <h1>It works!</h1>
+    <div>
+      <span>You're ready to start!</span>
+      <span>Skeleton 2, jQuery 2, and AWS Libraries are included.</span>
+    </div>
+  </div>;
+
+export default App;
