8074a00b5110b672023b40a70f095792e911005c
otofu-square <yukita@feedforce.jp>
2017-06-30 01:33:55 +0900

    :tada: Introduce React app on S3

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    // Directory /client/dist is used by serverless-client-s3
+    path: __dirname + '/client/dist',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/client/assets/index.html',
+    }),
+  ],
+};
