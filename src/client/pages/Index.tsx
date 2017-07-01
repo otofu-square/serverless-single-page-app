cc57190351503a9697349b671db38872d3bec15c
otofu-square <yukita@feedforce.jp>
2017-07-01 23:38:00 +0900

    :+1: Add view-container class

--- src/client/pages/Index.tsx
+++ src/client/pages/Index.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 
 const Index = () =>
-  <div className="container">
-    <div className="row">
-      <div className="one-half column">
-        <h3>Learn JavaScript, one puzzle at a time.</h3>
-        <a href="" className="button button-primary">
-          Start Now!
-        </a>
-      </div>
-      <div className="one-half column">
-        <img src={require('./assets/HeroImage.jpg')} />
+  <div className="markup">
+    <div className="view-container container">
+      <div className="row">
+        <div className="one-half column">
+          <h3>Learn JavaScript, one puzzle at a time.</h3>
+          <a href="" className="button button-primary">
+            Start Now!
+          </a>
+        </div>
+        <div className="one-half column">
+          <img src={require('./assets/HeroImage.jpg')} />
+        </div>
       </div>
     </div>
   </div>;
