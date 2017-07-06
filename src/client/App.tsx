791b9640257db8e304b906cd1fe45734fc992756
otofu-square <yukita@feedforce.jp>
2017-07-07 01:45:33 +0900

    :+1: Add react-router-redux, redux-thunk

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,26 +1,23 @@
 import * as React from 'react';
-import { createStore, Reducer } from 'redux';
-import { Provider } from 'react-redux';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
+import createHistory from 'history/createHashHistory';
 
 import Index from './pages/Index';
-import Problem from './containers/problem';
-import { problemsReducer } from './reducers/problems';
-
-const store = createStore(problemsReducer as any);
+import Problem from './containers/ProblemDetail';
+import { history } from './store';
 
 const App = () =>
-  <Provider store={store}>
+  <ConnectedRouter history={history}>
     <div className="markup">
       <div className="view-container container">
-        <HashRouter>
-          <Switch>
-            <Route exact path="/" component={Index} />
-            <Route path="/problem/:id" component={Problem as any} />
-          </Switch>
-        </HashRouter>;
+        <Switch>
+          <Route exact path="/" component={Index as any} />
+          <Route path="/problem/:id" component={Problem as any} />
+        </Switch>
       </div>
-    </div>;
-  </Provider>;
+    </div>
+  </ConnectedRouter>;
 
 export default App;
