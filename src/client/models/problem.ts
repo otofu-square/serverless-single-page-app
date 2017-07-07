f0481b4983affa45891cd10b338c1bca9adcd02b
otofu-square <yukita@feedforce.jp>
2017-07-08 00:20:48 +0900

    :+1: Add getProblemById function

--- src/client/models/problem.ts
+++ src/client/models/problem.ts
@@ -1,4 +1,10 @@
+import { filter, head, pipe } from 'ramda';
+
 export interface Problem {
+  id: number;
   description: string;
   code: string;
 }
+
+export const getProblemById = (problems: Problem[], id: number) =>
+  head(filter((p: Problem) => p.id === id, problems));
